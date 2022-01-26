c682ee9984a1868727d6a406fd74cfd08251b2c8
carolzelima <carol.gha@gmail.com>
2022-01-26 19:47:44 -0300

    update project

---
+++ script.js
@@ -0,0 +1,28 @@
+//1 - Criar um loop que conte de 1 até 10 usando FOR 
+console.log("for 1 to 10")
+for(let number=1; number<=10; number++) {
+    console.log(number)
+}
+    
+
+//2 - Criar um loop que conte de 10 até 1 usando WHILE
+let $number = 1;
+console.log("while 1 to 10")
+while($number <=10) {
+    console.log($number)
+    $number+=1
+}
+
+//3 - Criar um loop que conte todos os números ímpares de 1 até 100
+console.log("odd numbers using for - 1 to 100")
+for(let number=1; number<=100; number+=2) {
+    console.log(number)
+} 
+
+//4- Criar um loop que conte todos os números pares de 0 a 100
+let _number = 0;
+console.log("pair numbers using while - 0 to 100")
+while(_number <=100) {
+    console.log(_number)
+    _number+=2
+}    
